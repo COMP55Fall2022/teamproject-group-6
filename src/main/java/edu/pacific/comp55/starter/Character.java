2add0291da3e568d566ca4aac363f930af0ad413
Xuan Tan <x_tan2@u.pacific.edu>
2022-11-02 16:11:45 -0700

    create privates

--- src/main/java/edu/pacific/comp55/starter/Character.java
+++ src/main/java/edu/pacific/comp55/starter/Character.java
@@ -1,5 +1,19 @@
 package edu.pacific.comp55.starter;
 
+import java.util.ArrayList;
+
 public class Character {
 	private CharacterType type;
+	private int startX;
+	private int startY;
+	private int width;
+	private int height;
+	private int health;
+	private int rateOfFire;
+	private int speed;
+	private int speedOfBullet;
+	private int damage;
+	private ArrayList<Character> characters = new ArrayList();
+	
+	
 }
