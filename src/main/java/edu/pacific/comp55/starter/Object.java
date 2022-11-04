fa7f71831444b98d481f9545e484e0779da086bf
Ryan Phan <r_nguyen18@u.pacific.edu>
2022-11-04 16:17:06 -0700

    1st Commit and Push for Object

--- src/main/java/edu/pacific/comp55/starter/Object.java
+++ src/main/java/edu/pacific/comp55/starter/Object.java
@@ -1,5 +1,25 @@
 package edu.pacific.comp55.starter;
 
 public class Object {
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	
+	public int getWidth() {
+		return width;
+	}
 
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
 }
