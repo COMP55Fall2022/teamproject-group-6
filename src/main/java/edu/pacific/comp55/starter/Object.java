8b11f393f3f8e35d7eae2325405cb11902c337a6
Ryan Phan <r_nguyen18@u.pacific.edu>
2022-11-06 23:06:01 -0800

    Object Reset

--- src/main/java/edu/pacific/comp55/starter/Object.java
+++ src/main/java/edu/pacific/comp55/starter/Object.java
@@ -1,48 +1,12 @@
 package edu.pacific.comp55.starter;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.GraphicsDevice.*;
+import java.util.ArrayList;
+import java.awt.Graphics;
+import java.awt.geom.Line2D;
 
 public class Object {
-	protected int x;
-	protected int y;
-	protected int width;
-	protected int height;
-	protected boolean visible;
-	
-	public Object(int x, int y) {
-		this.x = x;
-		this.y = y;
-		visible = true;
-	}
-	
-	public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
     
-	public int getWidth() {
-		return width;
-	}
-
-	public void setWidth(int width) {
-		this.width = width;
-	}
-	
-	public int getHeight() {
-		return height;
-	}
-
-	public void setHeight(int height) {
-		this.height = height;
-	}
-	
-	public boolean isVisible() {
-        return visible;
-    }
-	
-	public void setVisible(Boolean visible) {
-        this.visible = visible;
-    }
-	
 }
