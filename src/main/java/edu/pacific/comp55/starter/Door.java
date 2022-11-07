eea6b634f44da9af5ad25d8fc49ceea353a57efd
Leah Hernandez <l_hernandez23@u.pacific.edu>
2022-11-06 23:34:42 -0800

    added function setPosition in Door

--- src/main/java/edu/pacific/comp55/starter/Door.java
+++ src/main/java/edu/pacific/comp55/starter/Door.java
@@ -3,10 +3,57 @@ package edu.pacific.comp55.starter;
 import java.util.ArrayList;
 
 public class Door {
+	
+	private int screen;
+	private int startPoint;
+	public int position;
+	//setScreen("Screen1");
+	
+private ArrayList<Door> door = new ArrayList<Door>();
+		
+	for (int i = 0; i < door.size(); i++) {
+		
+	}
+	
+	// setters
+	
+	public void setPosition() {
+		// TODO
+	}
+		
+	public int getPosition() {
+		return position;
+	}
+		
+	private int getScreen() {
+		return screen;
+	}
+		
+
+/*
 
 	
+
 	
-	
-	
+	boolean isOpen = false;
+	if (isOpen = false) {
+		return isOpen;
+		else {
+			break;
+		}
+*/
+/*	
+newDoor;
+
+// get Position in character class
+//character position
+//bool true or false
+//
+
+DoorList.getPosition() {
+
+}
+if (position =  )
+*/
 	
 }
