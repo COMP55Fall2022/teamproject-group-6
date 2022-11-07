af0722372f807a45a11269da58b7e368baa9e1c0
Ryan Phan <r_nguyen18@u.pacific.edu>
2022-11-06 23:18:53 -0800

    Object C&P(1.2)

--- src/main/java/edu/pacific/comp55/starter/Object.java
+++ src/main/java/edu/pacific/comp55/starter/Object.java
@@ -8,23 +8,14 @@ public class Object {
 	private int width;
 	private int height;
 	private int health;
-	private int rateOfFire;
-	private int speed;
-	private int speedOfBullet;
-	private int damage;
 	private ArrayList<Character> characters = new ArrayList();
 	private Point[] collisionDetectionPoints = new Point[8];
     
-    public Object(int startX, int startY, int width, int height, int health, int rateOfFire, int speed, int speedOfBullet, int damage) {
+    public Object(int startX, int startY, int width, int height, int health) {
 		super();
 		this.startPoint = new Point(startX, startY);
 		this.width = width;
 		this.height = height;
-		this.health = health;
-		this.rateOfFire = rateOfFire;
-		this.speed = speed;
-		this.speedOfBullet = speedOfBullet;
-		this.damage = damage;
 		for (int i = 0; i < 8; ++i) {
 			collisionDetectionPoints[i] = new Point(0, 0);
 		}
