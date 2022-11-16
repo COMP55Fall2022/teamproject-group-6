b017552809e6543d8eb3e8ccfd06fe9567a43056
nazeemkamel <nazeemkamel@dhcp-10-15-143-92.wireless.pacific.edu>
2022-11-15 19:01:20 -0800

    Merge branch 'main' of https://github.com/COMP55Fall2022/teamproject-group-6.git into main
    set  up skeleton for 8 direction movement, finished function to traverse
    through monster array, set up bullet array list skeleton

--- src/main/java/edu/pacific/comp55/starter/Room.java
+++ src/main/java/edu/pacific/comp55/starter/Room.java
@@ -10,7 +10,7 @@ public class Room {
 	private RoomType type;
 	private boolean isCompleted;
 	private Character player;
-	private ArrayList<Character> characters = new ArrayList<Character>();
+	private ArrayList<Monster> monsters = new ArrayList<Monster>();
 	private ArrayList<Bullet> bullets = new ArrayList<Bullet>();
 	
 	//TODO: Constructor
@@ -21,20 +21,34 @@ public class Room {
 	
 	
 	
-	public void characterMovement(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_W) {
-			// TODO
-		}
-		if (e.getKeyCode() == KeyEvent.VK_A) {
-			// TODO
-		}
-		if (e.getKeyCode() == KeyEvent.VK_S) {
-			// TODO
-		}
-		if (e.getKeyCode() == KeyEvent.VK_D) {
-			// TODO
-		}
-	}
+//	public void characterMovement(KeyEvent e) {
+//		if(//if one key is pressed)	
+//			if (e.getKeyCode() == KeyEvent.VK_W) {
+//				// TODO
+//			}
+//			if (e.getKeyCode() == KeyEvent.VK_A) {
+//				// TODO
+//			}
+//			if (e.getKeyCode() == KeyEvent.VK_S) {
+//				// TODO
+//			}
+//			if (e.getKeyCode() == KeyEvent.VK_D) {
+//				// TODO
+//			}
+//		if(//if 2 keys are pressed)	
+//			if (e.getKeyCode() == KeyEvent.VK_W || e.getKeyCode() == KeyEvent.VK_A) {
+//				// TODO
+//			}
+//			if (e.getKeyCode() == KeyEvent.VK_A || e.getKeyCode() == KeyEvent.VK_S) {
+//				// TODO
+//			}
+//			if (e.getKeyCode() == KeyEvent.VK_S || e.getKeyCode() == KeyEvent.VK_D) {
+//				// TODO
+//			}
+//			if (e.getKeyCode() == KeyEvent.VK_D || e.getKeyCode() == KeyEvent.VK_W) {
+//				// TODO
+//			}
+//	}
 	
 	public void characterAbilities(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_Q) {
@@ -49,5 +63,17 @@ public class Room {
 		//TODO
 		//no more than 3 mouse clicks or key presses
 	}
+	public void traverseMonsterArraylist(ArrayList<Monster> list) {
+		for(Monster m:list) {
+			if(m.isDead() == true) {
+				list.remove(m);
+			}
+		}
+	}
 
+//	public void traverseBulletArraylist(ArrayList<Bullet> list) {
+//		for(Bullet b:list) {
+//			if(b.getCollisionDetectionPoints())
+//		}
+//	}
 }
