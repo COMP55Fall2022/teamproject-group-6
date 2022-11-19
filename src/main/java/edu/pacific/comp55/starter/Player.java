daaa0cbaec505911e276c6eebe2bd59ac53767e0
Leah Hernandez <l_hernandez23@u.pacific.edu>
2022-11-18 16:23:53 -0800

    Created variable selection

--- src/main/java/edu/pacific/comp55/starter/Player.java
+++ src/main/java/edu/pacific/comp55/starter/Player.java
@@ -2,9 +2,18 @@ package edu.pacific.comp55.starter;
 
 public class Player {
 
+	private int selection;
+	
 	//timer, save points, interaction, difficulty, upgrades
 	
+	
+	//Difficulty: increased damage, reduced health, less upgrades
+/*	
 	private void selectDiff() {
-		
+		if(selection == 0) {
+			
+		}
 	}
+
+*/
 }
