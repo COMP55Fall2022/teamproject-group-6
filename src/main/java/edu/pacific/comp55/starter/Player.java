e3fdb3c273fe0cd2762f23a1c8c1070296d7ea5a
Ryan Phan <r_nguyen18@u.pacific.edu>
2022-11-20 23:21:00 -0800

    add data type to Player class

--- src/main/java/edu/pacific/comp55/starter/Player.java
+++ src/main/java/edu/pacific/comp55/starter/Player.java
@@ -1,5 +1,14 @@
 package edu.pacific.comp55.starter;
+import java.io.Serializable;
 
-public class Player {
-
+public class Player implements Serializable {
+	int level;
+	int maxLife;
+	int strength;
+	int exteraHealth;
+	int shield;
+	int exp;
+	int nextLevelExp;
+	int coin;
+	
 }
