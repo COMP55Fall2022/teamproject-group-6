e0cdcdd25cab9e153cacd5e94cd75ca39c0f532d
Ryan Phan <r_nguyen18@u.pacific.edu>
2022-11-20 17:23:14 -0800

    Added Type for Object as comment

--- src/main/java/edu/pacific/comp55/starter/Object.java
+++ src/main/java/edu/pacific/comp55/starter/Object.java
@@ -4,24 +4,26 @@ import java.util.ArrayList;
 
 
 public class Object {
+	//private ObjectType type;
 	private Point startPoint;
 	private int width;
 	private int height;
 	private int health;
-	private Point[] collisionDetectionPoints = new Point[8];
-	private boolean showImage;
-	
+	private Point[] collisionDetectionPoints = new Point[8];	
     
     public Object(int startX, int startY, int width, int height, int health) {
-		super();
-		this.startPoint = new Point(startX, startY);
+		//this.type = type;
+    	this.startPoint = new Point(startX, startY);
 		this.width = width;
 		this.height = height;
 		for (int i = 0; i < 8; ++i) {
 			collisionDetectionPoints[i] = new Point(0, 0);
 		}
-		this.updateCollisionDetectionPoints();
-	}//123
+	}
+    
+    //public ObjectType getObjectType() {
+		//return type;
+	//}
     
     public int getStartX() {
 		return startPoint.getX();
@@ -63,18 +65,5 @@ public class Object {
 		this.health = health;
 	}
 	
-	public void updateCollisionDetectionPoints() {
-		collisionDetectionPoints[0] = this.startPoint;	
-		collisionDetectionPoints[1].setXY(getStartX(), getStartY() + this.height / 2);	
-		collisionDetectionPoints[2].setXY(getStartX(), getStartY() + this.height);	
-		collisionDetectionPoints[3].setXY(getStartX() + this.width / 2, getStartY() + this.height);	
-		collisionDetectionPoints[4].setXY(getStartX() + this.width, getStartY() + this.height);
-		collisionDetectionPoints[5].setXY(getStartX() + this.width, getStartY() + this.height / 2); 
-		collisionDetectionPoints[6].setXY(getStartX() + this.width, getStartY());
-		collisionDetectionPoints[7].setXY(getStartX() + this.width / 2, getStartY());
-	}
-    
-    public Point[] getCollisionDetectionPoints() {
-		return collisionDetectionPoints;
-	}
+	
 }
