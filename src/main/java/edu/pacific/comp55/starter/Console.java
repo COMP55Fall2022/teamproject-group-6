d3a57ab397a6e75fd295f065fa2c26f20820c8a8
Leah Hernandez <l_hernandez23@u.pacific.edu>
2022-11-20 23:08:50 -0800

    started function addButtons

--- src/main/java/edu/pacific/comp55/starter/Console.java
+++ src/main/java/edu/pacific/comp55/starter/Console.java
@@ -8,8 +8,21 @@ public abstract class Console extends MainApplication implements ActionListener 
 	
 
 
+private Object play;
+private Object settings;
+
+
+
+
 //	MButton play = new MButton("play");
 //	MButton settings = new MButton("settings");
 //	MButton exit = new MButton("exit");
 //	MButton mainMenu = new MButton("mainMenu");
+	
+	
+	
+	public void addButtons() {
+		play.addActionListener(this);
+		settings.addActionListener(this);
+	}
 }
