f80fcbc1a3dd1b9599fdd6a54440b987ea9af78b
Leah Hernandez <l_hernandez23@u.pacific.edu>
2022-11-27 23:55:43 -0800

    Added comments for code

--- src/main/java/edu/pacific/comp55/starter/Difficulty.java
+++ src/main/java/edu/pacific/comp55/starter/Difficulty.java
@@ -7,16 +7,19 @@ public class Difficulty {
 	private int health;
 	
 	public Difficulty(int damage, int health ) {
+		//Normal difficulty
 		if(selection == 0) {
 			this.damage = damage;
 			this.health = health;
 			
 		}
+		//Medium difficulty
 		else if(selection == 1) {
 			this.damage = damage + 1;
 			this.health = health - 1;
 		//remove certain upgrades
 		}
+		//Hard difficulty
 		else if(selection == 2) {
 			this.damage = damage + 2;
 			this.health = health - 2;
