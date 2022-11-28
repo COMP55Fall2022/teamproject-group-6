8a8c1243db6e10c0ff7aa5d500b16bc7a8af783e
Ryan Phan <r_nguyen18@u.pacific.edu>
2022-11-28 00:19:06 -0800

    public void save and public void load lines

--- src/main/java/edu/pacific/comp55/starter/SavaLoad.java
+++ src/main/java/edu/pacific/comp55/starter/SavaLoad.java
@@ -2,6 +2,10 @@ package edu.pacific.comp55.starter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+
+import org.checkerframework.checker.units.qual.s;
+
 import main.Console;
 
 public class SavaLoad {
@@ -15,6 +19,16 @@ public class SavaLoad {
 		try {
 			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File("save.dat")));
 			DataStorage ds = new DataStorage(); 
+			
+			ds.level = gp.player.level;
+			ds.maxLife = gp.player.maxLife;
+			ds.life = gp.player.life;
+			ds.strength = gp.player.strength;
+			ds.exp = gp.player.exp;
+			ds.coin = gp.player.coin;
+			
+			oos.writeObject(ds);
+			
 		}
 		catch(Exception e) {
 			System.out.println("Save Exception!");
@@ -22,6 +36,18 @@ public class SavaLoad {
 	}
 		
 	public void load() {
-		
+		try {
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File("save.dat")));
+			DataStorage ds = (DataStorage)ois.readObject();
+			
+			gp.player.level = ds.level;
+			gp.player.maxLife = ds.maxLife;
+			gp.player.life = ds.life;
+			gp.player.strength = ds.strength;
+			gp.player.exp = ds.exp;
+			gp.player.coin = ds.coin;
+		}
+		catch(Exception e) {
+			System.out.println("Load Exception!");
 	}
 }
