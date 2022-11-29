608386d386ec00cdc394e0e8fcdf12cfe63b8bf1
Leah Hernandez <l_hernandez23@u.pacific.edu>
2022-11-28 16:36:58 -0800

    Initialized variables

--- src/main/java/edu/pacific/comp55/starter/PresetRoom.java
+++ src/main/java/edu/pacific/comp55/starter/PresetRoom.java
@@ -1,5 +1,19 @@
 package edu.pacific.comp55.starter;
 
 public class PresetRoom {
-
+	
+	public void getCharacterType(CharacterType charType, Object object) {
+		
+		Object PLAYER = null;
+		Object characterType = null;
+		
+		
+			
+	}
+	
+	
+	
+	//add monsters
+	//add objects
+	
 }
