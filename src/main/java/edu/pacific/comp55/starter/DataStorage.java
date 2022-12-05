2259e0723352c734353f24876540c2292e9531be
Xuan Tan <x_tan2@u.pacific.edu>
2022-12-04 20:47:48 -0800

    implement create save file function

--- src/main/java/edu/pacific/comp55/starter/DataStorage.java
+++ src/main/java/edu/pacific/comp55/starter/DataStorage.java
@@ -1,4 +1,6 @@
 package edu.pacific.comp55.starter;
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 
@@ -21,4 +23,24 @@ public class DataStorage implements Serializable{
 	int mapObjectWorldY[][];
 	String mapObjectLootName[][];
 	boolean mapObjectOpened[][];
+	
+	public void createSaveFile() {
+		try {
+		      File myObj = new File("save.txt");
+		      if (myObj.createNewFile()) {
+		        System.out.println("File created: " + myObj.getName());
+		      } else {
+		        System.out.println("File already exists.");
+		      }
+		    } catch (IOException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+	}
+	
+	//Test
+	public static void main(String[] args) {
+		DataStorage d1 = new DataStorage();
+		d1.createSaveFile();
+	}
 }
