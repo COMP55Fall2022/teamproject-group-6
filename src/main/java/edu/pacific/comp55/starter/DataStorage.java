b2ab712a6849f1cdd94daf9ab10d53b7f6c51776
Xuan Tan <x_tan2@u.pacific.edu>
2022-12-04 21:23:22 -0800

    Complete readSaveFile function

--- src/main/java/edu/pacific/comp55/starter/DataStorage.java
+++ src/main/java/edu/pacific/comp55/starter/DataStorage.java
@@ -1,19 +1,23 @@
 package edu.pacific.comp55.starter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class DataStorage implements Serializable{
-	int level;
-	int maxLife;
-	int life; 
-	int strength;
-	int exteraHealth;
+	//int level;
+	int maxHP;
+	//int life; 
+	//int strength;
+	//int exteraHealth;
+	int damage;
 	int shield;
-	int exp;
-	int nextLevelExp;
+	//int exp;
+	//int nextLevelExp;
 	int coin;
+	int timeOfComplete = 0;
 	
 	ArrayList<String> itemNames = new ArrayList<>();
 	ArrayList<Integer> itemAmounts = new ArrayList<>();
@@ -23,24 +27,54 @@ public class DataStorage implements Serializable{
 	int mapObjectWorldY[][];
 	String mapObjectLootName[][];
 	boolean mapObjectOpened[][];
+	int dataRecieved[] = new int[5];
 	
 	public void createSaveFile() {
-		try {
+			try {
 		      File myObj = new File("save.txt");
 		      if (myObj.createNewFile()) {
-		        System.out.println("File created: " + myObj.getName());
-		      } else {
-		        System.out.println("File already exists.");
+		    	  System.out.println("File created: " + myObj.getName());
+			  } else {
+				  System.out.println("File already exists.");
+			  }
+			  } catch (IOException e) {
+				  System.out.println("An error occurred.");
+				  e.printStackTrace();
+			  }
+	}
+	
+	public void readSaveFile() {
+			try {
+		      File myObj = new File("save.txt");
+		      Scanner myReader = new Scanner(myObj);
+		      int count = 0;
+		      while (myReader.hasNextLine()) {
+		    	String data = myReader.nextLine();
+		        dataRecieved[count] = Integer.parseInt(data);
+		        count++;
 		      }
-		    } catch (IOException e) {
+		      myReader.close();
+		      maxHP = dataRecieved[0];
+		      damage = dataRecieved[1];
+		      shield = dataRecieved[2];
+		      coin = dataRecieved[3];
+		      timeOfComplete = dataRecieved[4];
+		      System.out.println("HP: " + maxHP + "\n" + "Damage: " + damage + "\n"  + "Shield: " + shield + "\n" 
+		      + "Coin: " + coin + "\n"  + "TimeOfComplete: " + timeOfComplete);
+		    } catch (FileNotFoundException e) {
 		      System.out.println("An error occurred.");
 		      e.printStackTrace();
 		    }
+	}
+	
+	public void writeSaveFile() {
+		
 	}
 	
 	//Test
 	public static void main(String[] args) {
 		DataStorage d1 = new DataStorage();
 		d1.createSaveFile();
+		d1.readSaveFile();
 	}
 }
