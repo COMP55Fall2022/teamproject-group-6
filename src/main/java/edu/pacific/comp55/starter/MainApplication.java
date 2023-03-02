ea43279eaf6b33bb65acaf198be58945c0a0800d
Nazeem Kamel <>
2023-03-01 18:49:34 -0800

    Load Rooms from a file

--- src/main/java/edu/pacific/comp55/starter/MainApplication.java
+++ src/main/java/edu/pacific/comp55/starter/MainApplication.java
@@ -1,5 +1,9 @@
 package edu.pacific.comp55.starter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import acm.graphics.GImage;
@@ -15,7 +19,7 @@ public class MainApplication extends GraphicsApplication {
 	private MenuPane menu;
 	private SettingPane setting;
 //	private EndingPane ending;
-	protected  Player player;
+	protected Player player;
 	private int count;
 	private DataStorage data = new DataStorage();
 
@@ -26,8 +30,8 @@ public class MainApplication extends GraphicsApplication {
 		setupRooms();
 		player.setRoom(rooms.get(0));
 	}
-	//comment to submit
-	
+	// comment to submit
+
 	public Player getPlayer() {
 		return player;
 	}
@@ -36,155 +40,130 @@ public class MainApplication extends GraphicsApplication {
 		this.player = player;
 	}
 
+	public Room loadRoom(int roomNumber, MainApplication screen, RoomType type, Player player, String configfile) {
+		try (BufferedReader br = Files.newBufferedReader(Paths.get(configfile))) {
+			ArrayList<Monster> monsters = new ArrayList<Monster>();
+			ArrayList<Object> objects = new ArrayList<Object>();
+			// read line by line
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] collumns = line.split(",");
+				if (collumns.length >= 6) {
+					switch (collumns[0].trim()) {
+					case "monster":
+
+						double x = Double.parseDouble(collumns[2]);
+
+						double y = Double.parseDouble(collumns[3]);
+
+						double width = Double.parseDouble(collumns[4]);
+
+						double height = Double.parseDouble(collumns[5]);
+
+						switch (collumns[1].trim()) {
+
+						case "chaser":
+							monsters.add(new MonsterChaser(x, y, width, height));
+							break;
+						case "sentry":
+							monsters.add(new MonsterSentry(x, y, width, height));
+							break;
+						case "patroller":
+							ArrayList<Point> path = new ArrayList<Point>();
+							for (int i = 6; i < collumns.length; i += 2) {
+								double h = Double.parseDouble(collumns[i]);
+
+								double g = Double.parseDouble(collumns[i + 1]);
+
+								path.add(new Point(h, g));
+							}
+							MonsterPatroller m = new MonsterPatroller(x, y, width, height);
+							m.setPath(path);
+							monsters.add(m);
+							break;
+						}
+						break;
+					case "object":
+						double ox = Double.parseDouble(collumns[2]);
+
+						double oy = Double.parseDouble(collumns[3]);
+
+						double owidth = Double.parseDouble(collumns[4]);
+
+						double oheight = Double.parseDouble(collumns[5]);
+						Object o = new Object(collumns[1].trim(), ox, oy, owidth, oheight);
+						objects.add(o);
+						break;
+					}
+				}
+			}
+			return new Room(roomNumber, screen, type, player, monsters, objects);
+		} 
+		catch (IOException e) {
+			System.err.format("IOException: %s%n", e);
+			return null;
+		}
+	}
+
 	public void setupRooms() {
 		rooms = new ArrayList<Room>();
 		ArrayList<Monster> monsters = new ArrayList<Monster>();
-
-		monsters.add(new MonsterChaser(100, 200, 50, 50));
-		monsters.add(new MonsterChaser(250, 425, 50, 50));
-		monsters.add(new MonsterChaser(400, 500, 50, 50));
-		monsters.add(new MonsterSentry(425, 150, 50, 50));
-		monsters.add(new MonsterSentry(500, 275, 50, 50));
-
-
-		ArrayList<Point> path = new ArrayList<Point>();
-		path.add(new Point(100, 100));
-		path.add(new Point(50, 100));
-		path.add(new Point(50, 50));
-		path.add(new Point(150, 50));
-		path.add(new Point(400, 100));
-		path.add(new Point(200, 100));
-		path.add(new Point(300, 300));
-		path.add(new Point(350, 300));
-		path.add(new Point(600, 100));
-		
-		ArrayList<Point> path2 = new ArrayList<Point>();
-		path2.add(new Point (50, 150));
-		path2.add(new Point (100, 150));
-		path2.add(new Point (100, 50));
-		path2.add(new Point (200, 50));
-		path2.add(new Point (400, 200));
-		path2.add(new Point (600, 200));
-		path2.add(new Point (600, 400));
-		path2.add(new Point (300, 400));
-		path2.add(new Point (150, 200));
-		
-		ArrayList<Point> path3 = new ArrayList<Point>();
-		path2.add(new Point (200, 175));
-		path2.add(new Point (200, 200));
-		path2.add(new Point (300, 200));
-		path2.add(new Point (300, 400));
-		path2.add(new Point (500, 450));
-		path2.add(new Point (350, 350));
-		path2.add(new Point (100, 100));
-		path2.add(new Point (50, 100));
-		path2.add(new Point (250, 250));
-		
-  		MonsterPatroller patroler1 = new MonsterPatroller(150, 400, 62.5, 62.5);
-		MonsterPatroller patroler2 = new MonsterPatroller(275, 150, 62.5, 62.5);
-		MonsterPatroller patroler3 = new MonsterPatroller(600, 500, 62.5, 62.5);
-
-		patroler1.setPath(path);
-		patroler2.setPath(path2);
-		patroler3.setPath(path3);
-		monsters.add(patroler1);
-		monsters.add(patroler2);
-		monsters.add(patroler3);
-		
-		ArrayList<Object> objects = new ArrayList<Object>();
-		
-		Object box = new Object("robot head.jpg", 300, 400, 50, 50);
-		objects.add(box);
-		box = new Object("robot head.jpg", 45, 510, 50, 50);
-		objects.add(box);
-
-		GImage background = new GImage("room1.png", 0, 0); 
-		add(background);
-		Room room1 = new Room(1, this, RoomType.EMPTY, player, monsters, objects);
-
+		Room room1 = loadRoom(1, this, RoomType.EMPTY, player, "room1.txt");
 		rooms.add(room1);
 		
-		monsters = new ArrayList<Monster>();
-		monsters.add(new MonsterChaser(500, 250, 50, 50));
-		monsters.add(new MonsterChaser(300, 300, 50, 50));
-		monsters.add(new MonsterSentry(200, 375, 50, 50));
-		monsters.add(new MonsterSentry(275, 200, 50, 50));
-		monsters.add(new MonsterSentry(350, 325, 50, 50));
-
-
-
-		objects = new ArrayList<Object>();
-		//box = new Object("robot head.jpg", 30, 40, 50, 50);
-		//objects.add(box);
-		box = new Object("robot head.jpg", 45, 100, 50, 50);
-		objects.add(box);
-		
-		Room room2 = new Room(2, this, RoomType.BOSS, player, monsters, objects);
+		Room room2 = loadRoom(2, this, RoomType.BOSS, player, "room1.txt");
 		rooms.add(room2);
 		
-		monsters = new ArrayList<Monster>();
-		monsters.add(new MonsterChaser(500, 250, 50, 50));
-		monsters.add(new MonsterChaser(300, 300, 50, 50));
-		monsters.add(new MonsterSentry(200, 375, 50, 50));
-		monsters.add(new MonsterSentry(275, 200, 50, 50));
-		monsters.add(new MonsterSentry(350, 325, 50, 50));
-		objects = new ArrayList<Object>();
-		box = new Object("robot head.jpg", 45, 100, 50, 50);
-		objects.add(box);
-		
-		Room room3 = new Room(3, this, RoomType.BOSS, player, monsters, objects);
+		Room room3 = loadRoom(3, this, RoomType.BOSS, player, "room1.txt");
 		rooms.add(room3);
 		
-		monsters = new ArrayList<Monster>();
-		monsters.add(new MonsterChaser(500, 250, 50, 50));
-		monsters.add(new MonsterChaser(300, 300, 50, 50));
-		monsters.add(new MonsterSentry(200, 375, 50, 50));
-		monsters.add(new MonsterSentry(275, 200, 50, 50));
-		monsters.add(new MonsterSentry(350, 325, 50, 50));
-		objects = new ArrayList<Object>();
-		box = new Object("robot head.jpg", 45, 100, 50, 50);
-		objects.add(box);
-		
-		Room room4 = new Room(4, this, RoomType.BOSS, player, monsters, objects);
+		Room room4 = loadRoom(4, this, RoomType.BOSS, player, "room1.txt");
 		rooms.add(room4);
 		
-		monsters = new ArrayList<Monster>();
-		monsters.add(new MonsterChaser(500, 250, 50, 50));
-		monsters.add(new MonsterChaser(300, 300, 50, 50));
-		monsters.add(new MonsterSentry(200, 375, 50, 50));
-		monsters.add(new MonsterSentry(275, 200, 50, 50));
-		monsters.add(new MonsterSentry(350, 325, 50, 50));
-		objects = new ArrayList<Object>();
-		box = new Object("robot head.jpg", 45, 100, 50, 50);
-		objects.add(box);
-		
-		Room room5 = new Room(5, this, RoomType.BOSS, player, monsters, objects);
+		Room room5 = loadRoom(5, this, RoomType.BOSS, player, "room1.txt");
 		rooms.add(room5);
 		
-		Room[] neighbors1 = {null, room2, null, null};
+	
+
+
+//		Room room2 = new Room(2, this, RoomType.BOSS, player, monsters, objects);
+//		rooms.add(room2);
+//
+//		monsters = new ArrayList<Monster>();
+//		monsters.add(new MonsterChaser(500, 250, 50, 50));
+//		monsters.add(new MonsterChaser(300, 300, 50, 50));
+//		monsters.add(new MonsterSentry(200, 375, 50, 50));
+//		monsters.add(new MonsterSentry(275, 200, 50, 50));
+//		monsters.add(new MonsterSentry(350, 325, 50, 50));
+//		objects = new ArrayList<Object>();
+//		box = new Object("robot head.jpg", 45, 100, 50, 50);
+//		objects.add(box);
+
+
+		Room[] neighbors1 = { null, room2, null, null };
 		room1.setNeighbors(neighbors1);
 
-		Room[] neighbors2 = {room3, null, null, room1};
+		Room[] neighbors2 = { room3, null, null, room1 };
 		room2.setNeighbors(neighbors2);
-		
-		Room[] neightbors3 = {null, null, room2, room4}; 
+
+		Room[] neightbors3 = { null, null, room2, room4 };
 		room3.setNeighbors(neightbors3);
-		
-		Room[] neightbors4 = {room5, room3, null, null}; 
+
+		Room[] neightbors4 = { room5, room3, null, null };
 		room4.setNeighbors(neightbors4);
-		
-		Room[] neightbors5 = {null, null, room4, null}; 
+
+		Room[] neightbors5 = { null, null, room4, null };
 		room5.setNeighbors(neightbors5);
 	}
 
 	public void run() {
-		//load();
+		// load();
 		menu = new MenuPane(this);
 		setting = new SettingPane(this);
-		//ending = new EndingPane(this);
+		// ending = new EndingPane(this);
 		setupInteractions();
 		switchToMenu();
-		
+
 		animate();
 	}
 
@@ -193,7 +172,7 @@ public class MainApplication extends GraphicsApplication {
 		count++;
 		switchToScreen(menu);
 	}
-	
+
 	public void switchToSetting() {
 		switchToScreen(setting);
 	}
@@ -209,7 +188,7 @@ public class MainApplication extends GraphicsApplication {
 		AudioPlayer audio = AudioPlayer.getInstance();
 		audio.playSound(MUSIC_FOLDER, SOUND_FILES[count % SOUND_FILES.length]);
 	}
-	
+
 	public void save(Player player) {
 		data.maxHP = player.maxHP;
 //		data.damage = player.damage;
@@ -220,7 +199,8 @@ public class MainApplication extends GraphicsApplication {
 		}
 		data.writeSaveFile();
 	}
-	//merge
+
+	// merge
 	public void load() {
 		int dataRecieved[] = new int[5];
 		dataRecieved = data.readSaveFile();
@@ -231,7 +211,7 @@ public class MainApplication extends GraphicsApplication {
 		player.coin = dataRecieved[3];
 		player.time = dataRecieved[4];
 	}
-	
+
 	public static void main(String[] args) {
 		new MainApplication().start();
 	}
